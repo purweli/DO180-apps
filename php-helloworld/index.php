dce1d66752100f66ff082231e3eeb2d51bf0c8a8
Student User <student@workstation.lab.example.com>
2022-02-25 00:12:34 -0500

    Modified index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is in the air!.snart är det dags!" "\n";
+print "A change is in the air!.snart är det dags!";
 print "Yoohoo!!"
 ?>
